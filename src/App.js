3edde9683edc70d3f8f8fe97e689ee56b08719af
unknown <brosjayme@gmail.com>
2023-03-01 08:27:26 +0100

    simple to do app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Stack } from "react-bootstrap";
+import  Container  from "react-bootstrap/Container";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Container>
+<Stack direction="horizontal" gap={2} className="mb-4">
+  <h1 className="me-auto">Budgets</h1>
+  <button variant="primary">Add Budget</button>
+  <button variant="outline-primary">Add Expense</button>
+  
+</Stack>
+  </Container>
+   
 }
 
 export default App;
